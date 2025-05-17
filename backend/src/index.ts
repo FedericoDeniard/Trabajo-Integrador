0f962c6d6fe9f0d231603275ed91c5ef6ae85ccd
Federico <fededeniard@gmail.com>
2025-05-17 15:04:00 -0300

    Errorhandler middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,10 @@
 import { KEYS } from "./constants/keys";
 import express from "express";
+import { errorHandler } from "./middlewares/errorHandler";
 
 const app = express();
+
+app.use(errorHandler)
 
 app.listen(KEYS.PORT, () => {
     console.log(`Server running on port ${KEYS.PORT}`);
