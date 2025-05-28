6542ec3b0663c88015157dc63005d7cda4960b6a
Federico <fededeniard@gmail.com>
2025-05-27 22:50:34 -0300

    prettier configured in order to get more accurate commits from now

--- backend/src/routes/products.ts
+++ backend/src/routes/products.ts
@@ -1,10 +1,10 @@
-import express from "express";
-import { getProducts } from "../controllers/products";
-import { ResponseObject } from "../middlewares/errorHandler";
-import { Request, Response } from "express";
+import express from 'express';
+import { getProducts } from '../controllers/products';
+import { ResponseObject } from '../middlewares/errorHandler';
+import { Request, Response } from 'express';
 
 export const productsRouter = express.Router();
 
-productsRouter.get("/", (req: Request, res: Response) => {
-    res.json(new ResponseObject(true, getProducts(), "Products list successfully retrieved"))
-})        
+productsRouter.get('/', (req: Request, res: Response) => {
+  res.json(new ResponseObject(true, getProducts(), 'Products list successfully retrieved'));
+});
