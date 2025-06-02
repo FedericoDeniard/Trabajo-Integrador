539057a85d573380ddf5eaaf437894566dea0cfd
Delfina Garcia Ocampo <delfinagarciaoca06@gmail.com>
2025-06-02 20:57:50 -0300

    Card-media function

--- frontend/scripts/utils/productCard.js
+++ frontend/scripts/utils/productCard.js
@@ -1,12 +1,12 @@
-function createCardMedia({title: string,
-    date: Date, 
-    directors: string, 
-    rate: number, 
-    thumbnail: string, 
-    description: string, 
-    genres: string, 
-    price: number, 
-    seasons: number = null}) {
+function createCardMedia({title, date, directors, rate, thumbnail, description, genres, price, seasons = null}) {
+    if (!title) throw new Error("'title' no está definido");
+    if (!directors) throw new Error("'directors' no está definido");
+    if (rate === undefined || rate === null) throw new Error("'rate' no está definido");
+    if (!thumbnail) throw new Error("'thumbnail' no está definido");
+    if (!description) throw new Error("'description' no está definido");
+    if (!genres) throw new Error("'genres' no está definido");
+    if (price === undefined || price === null) throw new Error("'price' no está definido");
+    
     return `<div>
         <h2>${title}
         ${date ? `<em>(${date.getFullYear()})</em>` : ""}</h2>
@@ -24,10 +24,8 @@ function createCardMedia({title: string,
                 <button type="button">+</button>
             </div>
 
-            <p>$<span>${price}</span><p>
+            <p>$<span>${price}</span></p>
             <button type="submit">Agregar al carrito</button>
         </form>
     </div>`
 }
-
-console.log(createCardMedia({title:'si', date: new Date(2222,2,2), directors: 'juan', rate: 3.4, thumbnail:'https://prueba.com', description:'x', genres:'x', price:5, seasons:6}))
