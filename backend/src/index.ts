7bc429b16daba2631193ee5db5e2de6798377b4b
Federico <fededeniard@gmail.com>
2025-06-03 13:08:21 -0300

    database is now a class

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,7 @@ import { KEYS } from "./constants/keys";
 import express from "express";
 import { errorHandler } from "./middlewares/errorHandler";
 import { productsRouter } from "./routes/products";
-import prismaInstance, { gracefulShutdown } from "./services/db";
+import prismaInstance from "./services/db";
 
 
 
@@ -17,21 +17,16 @@ app.use(errorHandler)
 
 
 async function main() {
-    try {
-        await prismaInstance.$connect();
-        console.log("Database connected");
+  try {
+    await prismaInstance.connect();
+    app.listen(KEYS.PORT, () => {
+      console.log(`Server running on http://localhost:${KEYS.PORT}`)
+    });
 
-        app.listen(KEYS.PORT, () => {
-            console.log(`Server running on http://localhost:${KEYS.PORT}`)
-        });
 
-        process.on("SIGTERM", gracefulShutdown);
-        process.on("SIGINT", gracefulShutdown);
-
-    } catch (error) {
-        console.error("Database connection error", error);
-        process.exit(1);
-    }
+  } catch (error) {
+    process.exit(1);
+  }
 }
 
 main();
--- backend/src/services/db.ts
+++ backend/src/services/db.ts
@@ -1,18 +1,54 @@
 import { PrismaClient } from "../../generated/prisma"
 
-const prismaInstance = new PrismaClient({ log: ["info"] })
 
-export const gracefulShutdown = async (signal: string) => {
-    console.log(`Received ${signal}`);
-    try {
-        await prismaInstance.$disconnect();
-        console.log("Database disconnected")
+
+class PrismaService {
+    private prisma: PrismaClient;
+    private isConnected = false;
+
+    constructor() {
+        this.prisma = new PrismaClient({ log: ["info"] })
+        process.on("SIGTERM", () => this.gracefulShutdown("SIGTERM"));
+        process.on("SIGINT", () => this.gracefulShutdown("SIGINT"));
     }
-    catch (error) {
-        console.log(error);
-    } finally {
-        process.exit(0);
+
+
+    async connect() {
+        if (!this.isConnected) {
+            try {
+                await this.prisma.$connect();
+                this.isConnected = true;
+                console.log("Database connected");
+            } catch (error) {
+                console.error("DB connection error:", error);
+                throw error;
+            }
+        }
+        else {
+            console.log("Database already connected");
+        }
     }
+
+    async gracefulShutdown(signal: string) {
+        console.log(`Received ${signal}, disconnecting DB...`);
+        try {
+            await this.prisma.$disconnect();
+            console.log("Database disconnected");
+        } catch (error) {
+            console.error("Error disconnecting DB:", error);
+        } finally {
+            process.exit(0);
+        }
+    }
+
+    get client() {
+        if (!this.isConnected) {
+            throw new Error("Prisma client not connected");
+        }
+        return this.prisma;
+    }
+
 }
+const prismaInstance = new PrismaService()
 
 export default prismaInstance
