e5d4f5fa5bf1dba2a7aa149a75ec3c0484c1d87c
Federico <fededeniard@gmail.com>
2025-06-03 20:47:26 -0300

    Removed the cart test

--- frontend/scripts/mvc/cart/index.js
+++ frontend/scripts/mvc/cart/index.js
@@ -2,14 +2,4 @@ import cart from "../../utils/cart.js";
 import Controller from "./controller.js";
 
 const controller = new Controller();
-cart.addProduct({ id: 1, ammount: 1 });
-cart.addProduct({ id: 2, ammount: 1 });
-cart.addProduct({ id: 3, ammount: 1 });
-cart.addProduct({ id: 4, ammount: 1 });
-cart.addProduct({ id: 5, ammount: 1 });
-cart.addProduct({ id: 6, ammount: 1 });
-cart.addProduct({ id: 7, ammount: 1 });
-cart.addProduct({ id: 8, ammount: 1 });
-cart.addProduct({ id: 9, ammount: 1 });
-cart.addProduct({ id: 10, ammount: 1 });
 controller.init();
