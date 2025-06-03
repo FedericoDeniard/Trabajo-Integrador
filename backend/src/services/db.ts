a37d7c2403cfba3ff10ecc09c5acf860ec33479f
Federico <fededeniard@gmail.com>
2025-06-03 13:49:43 -0300

    Endpoint to get products

--- backend/src/services/db.ts
+++ backend/src/services/db.ts
@@ -1,7 +1,6 @@
+import { MediaWithRelations } from "src/controllers/products";
 import { PrismaClient } from "../../generated/prisma"
-
-
-
+import { HttpError } from "src/middlewares/errorHandler";
 class PrismaService {
     private prisma: PrismaClient;
     private isConnected = false;
@@ -11,7 +10,6 @@ class PrismaService {
         process.on("SIGTERM", () => this.gracefulShutdown("SIGTERM"));
         process.on("SIGINT", () => this.gracefulShutdown("SIGINT"));
     }
-
 
     async connect() {
         if (!this.isConnected) {
@@ -48,7 +46,43 @@ class PrismaService {
         return this.prisma;
     }
 
+    async getProductsByIds(ids: number[]): Promise<MediaWithRelations[]> {
+        try {
+            return this.client.media.findMany({
+                where: {
+                    id: {
+                        in: ids
+                    }
+                },
+                include: {
+                    genres: { include: { genre: true } },
+                    directors: { include: { director: true } },
+                    movie: true,
+                    serie: { include: { seasons: true } }
+                }
+            })
+        } catch (error) {
+            throw new HttpError(500, "Error retrieving products by ids");
+        }
+    }
+
+    async getAllProducts(): Promise<MediaWithRelations[]> {
+        try {
+            return this.client.media.findMany({
+                include: {
+                    genres: { include: { genre: true } },
+                    directors: { include: { director: true } },
+                    movie: true,
+                    serie: { include: { seasons: true } }
+                }
+            })
+        } catch (error) {
+            throw new HttpError(500, "Error retrieving all products");
+        }
+    }
+
 }
+
 const prismaInstance = new PrismaService()
 
 export default prismaInstance
