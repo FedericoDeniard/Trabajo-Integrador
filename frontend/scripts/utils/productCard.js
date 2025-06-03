fd98820354993e00c35d457be68327bda4d29e27
Federico <fededeniard@gmail.com>
2025-06-03 16:19:33 -0300

    product card fixed

--- frontend/scripts/utils/productCard.js
+++ frontend/scripts/utils/productCard.js
@@ -1,31 +1,47 @@
-function createCardMedia({title, date, directors, rate, thumbnail, description, genres, price, seasons = null}) {
-    if (!title) throw new Error("'title' no está definido");
-    if (!directors) throw new Error("'directors' no está definido");
-    if (rate === undefined || rate === null) throw new Error("'rate' no está definido");
-    if (!thumbnail) throw new Error("'thumbnail' no está definido");
-    if (!description) throw new Error("'description' no está definido");
-    if (!genres) throw new Error("'genres' no está definido");
-    if (price === undefined || price === null) throw new Error("'price' no está definido");
-    
-    return `<div>
+export default function createCardMedia({
+  title,
+  date,
+  directors,
+  rate,
+  thumbnail,
+  description,
+  genres,
+  price,
+  seasons = null,
+  ammount = 0,
+  cart = false,
+}) {
+  if (!title) throw new Error("'title' no está definido");
+  if (!directors) throw new Error("'directors' no está definido");
+  if (rate === undefined || rate === null)
+    throw new Error("'rate' no está definido");
+  if (!thumbnail) throw new Error("'thumbnail' no está definido");
+  if (!description) throw new Error("'description' no está definido");
+  if (!genres) throw new Error("'genres' no está definido");
+  if (price === undefined || price === null)
+    throw new Error("'price' no está definido");
+
+  return `<div>
         <h2>${title}
         ${date ? `<em>(${date.getFullYear()})</em>` : ""}</h2>
-        <p>${directors}</p>
+        <p>${directors.map((d) => d.director.name).join(", ")}</p>
         ${seasons ? `<p>${seasons} temporadas</p>` : ""}
         <span>${rate}</span>
         <img src="${thumbnail}" alt="${title}"/>
         <p>Descripción: ${description}</p>
-        <p>Géneros: ${genres}</p>
+        <p>Géneros: ${genres.map((g) => g.genre.name).join(", ")}</p>
         
         <form>
             <div>
                 <button type="button">-</button>
-                <input type="number" value="1" min="1" max="5" disabled/>
+                <input type="number" value="${ammount}" min="1" max="5" />
                 <button type="button">+</button>
             </div>
 
             <p>$<span>${price}</span></p>
-            <button type="submit">Agregar al carrito</button>
+            <button type="submit">${
+              !cart ? "Quitar del carrito" : "Agregar al carrito"
+            }</button>
         </form>
-    </div>`
+    </div>`;
 }
