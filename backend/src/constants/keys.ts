47a0e5417331f7c713168b7b60a7b6d8cd8005af
Federico <fededeniard@gmail.com>
2025-06-11 21:31:41 -0300

    ticket can be downloaded as pdf

--- backend/src/constants/keys.ts
+++ backend/src/constants/keys.ts
@@ -1,4 +1,5 @@
 export const KEYS = {
     PORT: process.env.PORT || 3000,
-    URL_BASE: process.env.URL_BASE || "http://localhost"
+    URL_BASE: process.env.URL_BASE || "http://localhost",
+    JWT_SECRET: process.env.JWT_SECRET || "secret"
 }
