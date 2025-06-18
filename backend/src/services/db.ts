7784976e3c16946480c48728af1052662020fe03
Federico <fededeniard@gmail.com>
2025-06-18 18:31:36 -0300

    function to map medias to either Serie or Movie, and a function to get mediaById

--- backend/src/services/db.ts
+++ backend/src/services/db.ts
@@ -24,6 +24,10 @@ export type SerieWithMedia = Prisma.SerieGetPayload<{
     }
 }>
 
+type MediaWithProduct = Prisma.MediaGetPayload<{
+    include: { Movie: true, Serie: { include: { seasons: true } }, genres: { include: { genre: true } }, directors: { include: { director: true } } }
+}>
+
 export type MediaByIdsResult = MovieWithMedia | SerieWithMedia
 
 class PrismaService {
@@ -96,6 +100,26 @@ class PrismaService {
         }
     }
 
+    async getMediaById(id: number): Promise<MediaByIdsResult> {
+        try {
+            const media = await this.client.media.findUnique({
+                where: {
+                    id: id
+                },
+                include: { Movie: true, Serie: { include: { seasons: true } }, genres: { include: { genre: true } }, directors: { include: { director: true } } }
+            })
+            let mappedMedia: MediaByIdsResult
+            if (media?.Movie) {
+                mappedMedia = this.mediaToMovie(media)
+            } else if (media?.Serie) {
+                mappedMedia = this.mediaToSerie(media)
+            } else throw new Error("Media not found");
+            return mappedMedia
+        } catch (error) {
+            throw new HttpError(500, "Error retrieving products by ids");
+        }
+    }
+
     async getAllProducts(): Promise<MediaByIdsResult[]> {
         try {
             const [movies, series] = await Promise.all([
@@ -111,6 +135,52 @@ class PrismaService {
             throw new HttpError(500, "Error retrieving products");
         }
     }
+
+    private mediaToSerie = (media: MediaWithProduct): SerieWithMedia => {
+        if (!media.Serie) throw new Error("Media is not a serie");
+        const serie = {
+            id: media.Serie.id,
+            seasons: media.Serie.seasons,
+            mediaId: media.id,
+            media: {
+                id: media.id,
+                title: media.title,
+                price: media.price,
+                thumbnail: media.thumbnail,
+                description: media.description,
+                rate: media.rate,
+                available: media.available,
+                genres: media.genres,
+                directors: media.directors
+            }
+
+        }
+        return serie
+    }
+
+    private mediaToMovie = (media: MediaWithProduct): MovieWithMedia => {
+        if (!media.Movie) throw new Error("Media is not a movie");
+        const movie = {
+            id: media.Movie.id,
+            duration: media.Movie.duration,
+            released_date: media.Movie.released_date,
+            mediaId: media.id,
+            media: {
+                id: media.id,
+                title: media.title,
+                price: media.price,
+                thumbnail: media.thumbnail,
+                description: media.description,
+                rate: media.rate,
+                available: media.available,
+                genres: media.genres,
+                directors: media.directors
+            }
+
+        }
+        return movie
+    }
+
 }
 
 const prismaInstance = new PrismaService()
