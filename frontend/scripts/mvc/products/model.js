71d06c00ec8bdcecb11f462e7c279f6973eb0dfd
Federico <fededeniard@gmail.com>
2025-06-27 12:02:50 -0300

    frontend pagination

--- frontend/scripts/mvc/products/model.js
+++ frontend/scripts/mvc/products/model.js
@@ -2,25 +2,39 @@ import cookiesReader from "../../utils/getCookies.js";
 
 class Model {
   products = [];
+  currentPage = 1;
+  pageSize = 5;
+  totalPages = 0;
+  totalProducts = 0;
+  hasMore = false;
+  hasPrevious = false;
   urlBase = cookiesReader.urlBase;
 
   async getProducts() {
-    const products = await this.#fetchProducts();
-    this.products = products.map(p => ({ ...p, amount: 1 }));
-    return products;
+    const response = await this.#fetchProducts();
+    this.products.push(...response.products.map((p) => ({ ...p, amount: 1 })));
+    return this.products;
   }
 
   async #fetchProducts() {
-    const response = await fetch(`${this.urlBase}/api/products`);
+    const response = await fetch(
+      `${this.urlBase}/api/products/paginated?page=${this.currentPage}&limit=${this.pageSize}`
+    );
     if (!response.ok) {
       throw new Error("No se pudo obtener los productos");
     }
     const result = await response.json();
+    this.hasMore = result.data.hasMore;
+    this.currentPage = result.data.currentPage;
+    this.pageSize = result.data.pageSize;
+    this.totalPages = result.data.totalPages;
+    this.totalProducts = result.data.totalProducts;
+    this.hasPrevious = result.data.hasPrevious;
     return result.data;
   }
 
   decreaseAmount(productId) {
-    const product = this.products.find(p => p.mediaId === productId);
+    const product = this.products.find((p) => p.mediaId === productId);
     if (product) {
       if (product.amount > 1) {
         product.amount--;
@@ -29,17 +43,29 @@ class Model {
   }
 
   increaseAmount(productId) {
-    const product = this.products.find(p => p.mediaId === productId);
+    const product = this.products.find((p) => p.mediaId === productId);
     if (product) {
       product.amount++;
     }
   }
 
   resetAmount(productId) {
-    const product = this.products.find(p => p.mediaId === productId);
-    if (product){
+    const product = this.products.find((p) => p.mediaId === productId);
+    if (product) {
       product.amount = 1;
     }
+  }
+
+  async nextPage() {
+    this.currentPage++;
+    this.products = [];
+    return await this.getProducts();
+  }
+
+  async previousPage() {
+    this.currentPage--;
+    this.products = [];
+    return await this.getProducts();
   }
 }
 
--- frontend/scripts/mvc/products/view.js
+++ frontend/scripts/mvc/products/view.js
@@ -8,6 +8,8 @@ class View {
     this.$chboxSeries = this.$("series");
     this.$loader = this.$("general-loader");
     this.$logout = this.$("logout");
+    this.$prevPage = this.$("prev-page");
+    this.$nextPage = this.$("next-page");
   }
 
   $(id) {
@@ -25,15 +27,15 @@ class View {
       return;
     }
 
-    if(showPeliculas) {
-      htmlText += this.#filterMedia(products.filter(p => p.duration));
+    if (showPeliculas) {
+      htmlText += this.#filterMedia(products.filter((p) => p.duration));
     }
 
-    if(showSeries) {
-      htmlText += this.#filterMedia(products.filter(p => p.seasons));;
+    if (showSeries) {
+      htmlText += this.#filterMedia(products.filter((p) => p.seasons));
     }
 
-    if(htmlText == "") {
+    if (htmlText == "") {
       htmlText += "<p class='no-products'>Elige una categoría</p>";
     }
 
@@ -41,8 +43,8 @@ class View {
   }
 
   updateCard(card, product) {
-    const input = card.querySelector('.amount');
-    const priceSpan = card.querySelector('.price-span');
+    const input = card.querySelector(".amount");
+    const priceSpan = card.querySelector(".price-span");
 
     const amount = product.amount;
     input.value = amount;
@@ -51,25 +53,25 @@ class View {
   }
 
   showLoader() {
-    this.$products.style.display = 'none';
-    this.$loader.style.display = 'flex';
-    this.$loader.style.flex = '1';
+    this.$products.style.display = "none";
+    this.$loader.style.display = "flex";
+    this.$loader.style.flex = "1";
   }
 
   hideLoader() {
-    this.$products.style.display = 'flex';
-    this.$loader.style.display = 'none';
+    this.$products.style.display = "flex";
+    this.$loader.style.display = "none";
   }
 
   updateCartCount(amount) {
     this.$cartCount.textContent = amount;
   }
-  
+
   #filterMedia(filteredProducts) {
     let htmlText = "";
     filteredProducts.forEach((p) => {
       htmlText += createCardMedia(p);
-    })
+    });
     return htmlText;
   }
 }
